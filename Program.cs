944128c29bb2e1a54cc4295ebdc484e7b98aefdb
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 21:21:05 -0300

    ATIVIDADE CONCLUIDA

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+string password;
+ 
+Console.Write("Digite sua senha: ");
+password = Convert.ToString(Console.ReadLine())!;
+ 
+if (password != Convert.ToString("1234abcd"))
+{
+    Console.WriteLine("Acesso negado");
+}
+else
+{
+    Console.WriteLine("Acesso permitido");
+}
